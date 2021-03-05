6a3bc27149149f047a6199cdc3b2c1fa0ac66bce
Michael-Czekanski <michal.cz2202@gmail.com>
2021-03-05 21:09:01 +0000

    Add simple about page

--- forum/urls.py
+++ forum/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="forum-home")
+    path('', views.home, name="forum-home"),
+    path('about/', views.about, name="forum-about"),
 ]
--- forum/views.py
+++ forum/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'forum/home.html')
+
+def about(request):
+    return render(request, 'forum/about.html')
