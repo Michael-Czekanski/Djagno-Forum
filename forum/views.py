b0214bddc40f125e3364950c71cac273876cc5e0
Michael-Czekanski <michal.cz2202@gmail.com>
2021-03-06 11:51:07 +0000

    Add displaying all topics on forum home

--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
 def home(request):
-    return render(request, 'forum/home.html')
+
+    content = {
+        'topics': Topic.objects.all()
+    }
+    return render(request, 'forum/home.html', content)
 
 def about(request):
     return render(request, 'forum/about.html')
