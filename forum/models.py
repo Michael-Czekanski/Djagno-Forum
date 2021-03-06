8dd6ff581e72fc2c0764d96595afd3f607364ad3
Michael-Czekanski <michal.cz2202@gmail.com>
2021-03-06 10:28:41 +0000

    Add Topic and Post models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+    creation_date = models.DateField(default=timezone.now)
+    created_by = models.ForeignKey(User, models.SET_NULL, null=True)
+
+class Post(models.Model):
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, models.SET_NULL, null=True)
