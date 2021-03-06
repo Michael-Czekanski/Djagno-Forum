5d2f89ec80d29ee10864b65df2015e75c514f0b4
Michael-Czekanski <michal.cz2202@gmail.com>
2021-03-06 14:52:37 +0000

    Add message after register success

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
@@ -7,6 +8,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Your account has been created. You are now able to log in')
             return redirect('forum-home')
     else:
         form = UserRegisterForm()
