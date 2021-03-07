9d76288e80ac6918d18cfd6d5c19b11b64e6c6c2
Michael-Czekanski <michal.cz2202@gmail.com>
2021-03-07 13:10:43 +0000

    Add topic url path

--- forum/urls.py
+++ forum/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="forum-home"),
     path('about/', views.about, name="forum-about"),
+    path('topic/<int:topic_id>/', views.topic, name="forum-topic"),
 ]
