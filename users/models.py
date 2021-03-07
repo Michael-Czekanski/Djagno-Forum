bedaefd08a3d08bf11adac7aa1af180a6aba0fd0
Michael-Czekanski <michal.cz2202@gmail.com>
2021-03-07 19:02:18 +0000

    Add removing old image after profile image update

--- users/models.py
+++ users/models.py
@@ -10,8 +10,22 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     def save(self, *args, **kwargs):
+        self._remove_old_image()
         super().save(*args, **kwargs)
         self._resize_image()
+
+    def _remove_old_image(self):
+        """Removes old profile image.
+
+        Removes old profile image before saving newly updated profile image.
+        """
+
+        try:
+            this = Profile.objects.get(pk=self.pk)
+            if this.image != self.image and this.image.name != 'default.jpg':
+                this.image.delete(save=False)
+        except Profile.DoesNotExist:
+            pass
 
     def _resize_image(self):
         """Checks if profile image is too big and resizes it if neccessary"""
